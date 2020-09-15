9a7776a6bc2909c1fb62b80419f9ee72f586fbf1
Nathan Pierce <connarpierce@gmail.com>
2020-09-15 09:35:54 -0400

    various fixes

--- Casks/anka-virtualization.rb
+++ Casks/anka-virtualization.rb
@@ -3,10 +3,10 @@ cask "anka-virtualization" do
   sha256 "3f8937ea296ff16e87940b541e9b8ff67ffa23fe3f8d341c9bf5ff699598f776"
 
   url "https://d1efqjhnhbvc57.cloudfront.net/Anka-#{version}.pkg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://veertu.com/downloads/anka-virtualization-latest"
+  appcast "https://ankadocs.veertu.com/docs/release-notes/"
   name "Anka Virtualization"
+  desc "Anka is a suite of software for creating and managing macOS VMs to run on top of Apple hardware and macOS. It enables the creation of or integration with existing container-like DevOps workflows. Unlike existing macOS virtualization solutions, Anka lets the macOS operating system manage virtual machines in the same manner as other native macOS applications. Anka virtualization uses macOS native Hypervisor features so it can run on any Apple hardware"
   homepage "https://veertu.com/"
-  desc "Anka is a suite of software for creating and managing macOS VMs to run on top of Apple hardware and macOS. It enables the creation of or integration with existing container-like DevOps workflows. Unlike existing macOS virtualization solutions, Anka lets the macOS operating system manage virtual machines in the same manner as other native macOS applications. Anka virtualization uses macOS native Hypervisor features so it can run on any Apple hardware."
 
   depends_on macos: ">= :yosemite"
 
@@ -19,10 +19,11 @@ cask "anka-virtualization" do
     "com.veertu.anka.lupd",
     "com.veertu.anka.ankakbd",
     "com.veertu.anka.ankanetd",
-  ],script: {
-      executable: "/Library/Application Support/Veertu/Anka/tools/uninstall.sh",
-      args:       ["-f"],
-      sudo:       true,
+  ], 
+  script: {
+    executable: "/Library/Application Support/Veertu/Anka/tools/uninstall.sh",
+    args:       ["-f"],
+    sudo:       true,
   }
 
   caveats do
