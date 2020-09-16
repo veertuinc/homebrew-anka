cf812b8256085818aa5483697b6a8625d8456a81
Nathan Pierce <connarpierce@gmail.com>
2020-09-16 09:54:46 -0400

    appcast change

--- Casks/anka-build-cloud-registry.rb
+++ Casks/anka-build-cloud-registry.rb
@@ -3,10 +3,10 @@ cask "anka-build-cloud-registry" do
   sha256 "5cd249263b1242a50394c1ee2325aa7e3c53d3b4b4f753e2b78e4c98d7861425"
 
   url "https://d1efqjhnhbvc57.cloudfront.net/AnkaRegistry-#{version}.pkg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://veertu.com/downloads/ankaregistry-mac-latest"
+  appcast "https://ankadocs.veertu.com/docs/release-notes/"
   name "Anka Build Cloud Registry"
+  desc "Anka is a suite of software for creating and managing macOS VMs to run on top of Apple hardware and macOS. The Anka Build Cloud includes a Controller (GUI + API) & Registry to integrate with commonly used CI systems like Github Actions, Jenkins, Teamcity, GitLab, and Buildkite. This is the registry only"
   homepage "https://veertu.com/"
-  desc "Anka is a suite of software for creating and managing macOS VMs to run on top of Apple hardware and macOS. The Anka Build Cloud includes a Controller (GUI + API) & Registry to integrate with commonly used CI systems like Github Actions, Jenkins, Teamcity, GitLab, and Buildkite. This is the registry only."
 
   depends_on macos: ">= :yosemite"
 
@@ -14,12 +14,13 @@ cask "anka-build-cloud-registry" do
 
   uninstall launchctl: [
     "com.veertu.anka.registry.plist",
-  ], delete: [
+  ], 
+  delete: [
     "/Library/Application Support/Veertu/Anka/bin/fvutil",
     "/Library/Application Support/Veertu/Anka/bin/ankaregctl",
     "/Library/Application Support/Veertu/Anka/bin/ankaregd",
     "/Library/Application Support/Veertu/Anka/bin/vmnetproxy",
-    "/Library/Application Support/Veertu/Anka/bin/vlaunchd"
+    "/Library/Application Support/Veertu/Anka/bin/vlaunchd",
   ]
 
   caveats do
