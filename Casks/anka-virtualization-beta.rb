c9b85654b534b28bbded349648afd38b9e4f6073
Daniel Bayley <daniel.bayley@me.com>
2020-11-14 23:13:03 +0000

    Add anka-virtualization-beta

---
+++ Casks/anka-virtualization-beta.rb
@@ -0,0 +1,45 @@
+cask "anka-virtualization-beta" do
+  version "2.3.0.118.166"
+  sha256 "144776f0d68af861e01a2809a938f044fda88fed12d4fb3a00f036800544d04d"
+
+  # d1efqjhnhbvc57.cloudfront.net/ was verified as official when first introduced to the cask
+  url "https://d1efqjhnhbvc57.cloudfront.net/Anka-#{version}.pkg"
+  appcast "https://ankadocs.veertu.com/docs/release-notes/"
+  name "Anka Virtualization"
+  desc "CLI tool for managing and creating macOS virtual machines"
+  homepage "https://veertu.com/"
+
+  conflicts_with cask: "anka-virtualization"
+  depends_on macos: ">= :yosemite"
+
+  pkg "Anka-#{version}.pkg"
+
+  uninstall launchctl: [
+    "com.veertu.nlimit",
+    "com.veertu.vlaunch",
+    "com.veertu.anka.lupd",
+    "com.veertu.anka.ankakbd",
+    "com.veertu.anka.ankanetd",
+  ],
+            script:    {
+              executable: "/Library/Application Support/Veertu/Anka/tools/uninstall.sh",
+              args:       ["-f"],
+              sudo:       true,
+            }
+
+  zap trash: [
+    "~/.anka",
+    "~/Library/Application Support/Veertu/Anka",
+    "~/Library/Logs/Anka",
+    "~/Library/Preferences/com.veertu.ankaview.plist",
+    "/Library/Application Support/Veertu/Anka",
+  ],
+      rmdir: [
+        "~/Library/Application Support/Veertu",
+        "/Library/Application Support/Veertu",
+      ]
+
+  caveats do
+    license "https://veertu.com/terms-and-conditions/"
+  end
+end
