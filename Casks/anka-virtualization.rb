79784a33066d977da18bed3adf8a1fdcd5aa50db
Nathan Pierce <norsegaud@gmail.com>
2022-10-18 11:58:04 -0400

    quick fix

--- Casks/anka-virtualization.rb
+++ Casks/anka-virtualization.rb
@@ -8,7 +8,7 @@ cask "anka-virtualization" do
   end
   on_arm do
     version "3.1.0.151"
-    sha256 "e600e8144f5ca5134aa94785bc9bbc567193b1065944573df9cc9daf7d8f796e"
+    sha256 "6648a9cd0be56fcde4b5c150b2a59e8302306c477ad1a335f1dd56cb8b42a0ce"
     depends_on macos: ">= :monterey"
   end
 
