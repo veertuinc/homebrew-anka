8e01cf8df16b489c17da57a8c49c4358f7aa78a1
Nathan <NorseGaud@users.noreply.github.com>
2024-09-09 07:54:11 -0500

    Update anka-build-cloud-registry.rb

--- Casks/anka-build-cloud-registry.rb
+++ Casks/anka-build-cloud-registry.rb
@@ -1,38 +1,31 @@
 cask "anka-build-cloud-registry" do
-  arch arm: "Arm64", intel: "Amd64"
+  arch arm: "arm64", intel: "amd64"
 
-  version "1.38.0-3989ee24"
-  sha256 arm:   "fae24091f010db75c5404ffcdf3e493cb5f1f9639de352b9151f3fa179d733bb",
-         intel: "61ea865f7376434fc073001317bbe19bd1ba5ef465721cfac14db379abfca09e"
+  version "1.42.0-92735895"
+  sha256 arm:   "06eeb8d35cd12f6afc54eced8c0d3d61ba82ea99331645f6b4ec2e6432227122",
+         intel: "575a1e42d5ee9371b0b3f14fe3ad19060be81f72b1461e9c50bf6b118b4b71ac"
 
-  pkg "AnkaRegistry#{arch}-#{version}.pkg"
-
-  url "https://downloads.veertu.com/anka/AnkaRegistry#{arch}--#{version}.pkg"
-  name "Anka Build Cloud Registry"
-  desc "Store Anka's virtual machines in a central location"
+  url "https://downloads.veertu.com/anka/anka-registry-#{arch}-#{version}.pkg"
+  name "Anka Build Cloud Controller"
+  desc "Anka virtual machine registry & API"
   homepage "https://veertu.com/"
 
   livecheck do
-    url "https://veertu.com/downloads/anka-build-cloud-registry-darwin-#{arch.downcase}-latest"
-    regex(/AnkaRegistry#{arch}[._-]?v?(\d+(?:\.\d+)*[._-]\h+)\.pkg/i)
+    url "https://veertu.com/downloads/anka-build-cloud-registry-darwin-#{arch}-latest"
+    regex(/anka-registry-#{arch}[._-]?v?(\d+(?:\.\d+)*[._-]\h+)\.pkg/i)
     strategy :header_match
   end
 
-  uninstall launchctl: [
-              "com.veertu.anka.registry.plist",
-              "com.veertu.anka.registry",
-            ],
-            pkgutil:   "com.veertu.anka.registry.pkg",
-            delete:    [
-              "/Library/Application Support/Veertu/Anka/bin/fvutil",
-              "/Library/Application Support/Veertu/Anka/bin/ankaregctl",
-              "/Library/Application Support/Veertu/Anka/bin/ankaregd",
-              "/Library/Application Support/Veertu/Anka/bin/vmnetproxy",
-              "/Library/Application Support/Veertu/Anka/bin/vlaunchd",
-            ]
+  pkg "anka-registry-#{arch}-#{version}.pkg"
 
-  zap trash: "/Library/Logs/Veertu/AnkaController",
-      rmdir: "/Library/Application Support/Veertu/Anka/registry"
+  uninstall script: {
+    executable: "/Library/Application Support/Veertu/Anka/tools/registry/uninstall.sh",
+    args:       ["-f"],
+    sudo:       true,
+  }
+
+  zap trash: "/Library/Logs/Veertu/AnkaRegistry",
+      rmdir: "/Library/Application Support/Veertu/Anka/anka-registry"
 
   caveats do
     license "https://veertu.com/terms-and-conditions/"
